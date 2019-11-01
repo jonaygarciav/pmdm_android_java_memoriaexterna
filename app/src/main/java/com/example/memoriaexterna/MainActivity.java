0fd4b70360518cfdd226b308e3ca0521739ca1db
jonaygarciav <jonaygarciav@gmail.com>
2019-11-01 10:36:55 +0000

    Commit inicial

---
+++ app/src/main/java/com/example/memoriaexterna/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.memoriaexterna;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
